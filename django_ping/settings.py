ee9e87f7519b3bc628fddce22275334af08175cf
Xico Petry <xico.petry@gmail.com>
2021-12-28 15:14:13 -0300

    Added requirements and adjustment of the Decouple

--- django_ping/settings.py
+++ django_ping/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'], cast=list)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'], cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
