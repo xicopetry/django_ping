e2b1133642e96ba235877ebe9c9aaf55ffc82fef
Xico Petry <xico.petry@gmail.com>
2022-02-15 15:02:03 -0300

    Added django-import-export

--- home/admin.py
+++ home/admin.py
@@ -2,12 +2,23 @@ from django.contrib import admin
 
 from home.models import HostType, Host
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class HostResource(resources.ModelResource):
+
+    class Meta:
+        model = Host
+        fields = ('name', 'ip_address', 'status', 'date', 'last_date_alive', 'host_type__name', 'rtt_avg_ms')
+
 
 @admin.register(Host)
-class HostAdmin(admin.ModelAdmin):
+class HostAdmin(ImportExportModelAdmin):
 
     list_display = ('name', 'ip_address', 'status', 'date', 'host_type', 'rtt_avg_ms')
     list_filter = ('host_type', 'status')
+    resource_class = HostResource
 
 
 @admin.register(HostType)
