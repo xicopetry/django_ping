778b22da27c9c080f72410eabc15bc535f600666
Xico Petry <xico.petry@gmail.com>
2022-02-15 14:35:42 -0300

    Adjusted the list_display order on Host admin

--- home/admin.py
+++ home/admin.py
@@ -6,7 +6,7 @@ from home.models import HostType, Host
 @admin.register(Host)
 class HostAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'ip_address', 'status', 'date', 'rtt_avg_ms', 'packets_lost', 'host_type')
+    list_display = ('name', 'ip_address', 'status', 'date', 'host_type', 'rtt_avg_ms')
     list_filter = ('host_type', 'status')
 
 
